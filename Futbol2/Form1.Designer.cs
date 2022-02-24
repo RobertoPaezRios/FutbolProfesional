d632c4fd21b02a5367a5c2f9ec4c5888437ffc2e
Roberto Paez <robepa2009@gmail.com>
2022-02-24 07:09:45 +0100

    v0.01

---
+++ Futbol2/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace Futbol2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.btnAddJug = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 196);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(775, 242);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(13, 13);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(228, 169);
+            this.checkedListBox1.TabIndex = 1;
+            // 
+            // btnAddJug
+            // 
+            this.btnAddJug.Location = new System.Drawing.Point(384, 13);
+            this.btnAddJug.Name = "btnAddJug";
+            this.btnAddJug.Size = new System.Drawing.Size(75, 23);
+            this.btnAddJug.TabIndex = 2;
+            this.btnAddJug.Text = "button1";
+            this.btnAddJug.UseVisualStyleBackColor = true;
+            this.btnAddJug.Click += new System.EventHandler(this.btnAddJug_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAddJug);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button btnAddJug;
+    }
+}
+
---
+++ Futbol2/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Futbol2
+{
+    public partial class Form1 : Form
+    {
+
+        SqlConnection conn = new SqlConnection("Data Source = localhost; Initial Catalog = futbol; Integrated Security = True");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conn.Open();
+
+            cargarTablaJug();
+            cargarPosDis();
+            //agregarJug("Cristiano Ronaldo", 7, 91);
+        }
+
+        public void cargarTablaJug()
+        {
+            SqlCommand comando = new SqlCommand("SELECT * FROM jugadores;", conn);
+            SqlDataAdapter adaptador = new SqlDataAdapter();
+            adaptador.SelectCommand = comando;
+            DataTable tabla = new DataTable();
+            adaptador.Fill(tabla);
+            dataGridView1.DataSource = tabla;
+        }
+        
+        public void agregarJug (string nombre, int dorsal, int puntuacion)
+        {
+            int idJug = 0;
+
+            string query = "INSERT INTO jugadores (nombre, dorsal, puntuacion, created_at, updated_at) VALUES ('" + nombre + "'," +
+                "'" + dorsal + "', '" + puntuacion + "', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);";
+            SqlCommand comando = new SqlCommand(query, conn);
+            comando.ExecuteNonQuery();
+            cargarTablaJug();
+
+            string query2 = "SELECT id FROM jugadores ORDER BY id DESC";
+            SqlCommand comando2 = new SqlCommand(query2, conn);
+            SqlDataReader Reader = comando2.ExecuteReader();
+
+            if (Reader.Read())
+            {
+                idJug = Int32.Parse(Reader[0].ToString());
+            } 
+            else
+            {
+                MessageBox.Show("ERROR: no se encontraron datos", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            Reader.Close();
+
+            SqlCommand comando3;
+
+            for (int i = 0; i < checkedListBox1.CheckedIndices.Count; i++)
+            {
+                string query3 = "INSERT INTO pivote_puestos_jugadores(puesto_id, jugador_id) VALUES ('" + Int32.Parse((checkedListBox1.CheckedIndices[i] + 1).ToString()) + "', '" + idJug.ToString() + "');";
+                comando3 = new SqlCommand(query3, conn);
+                comando3.ExecuteNonQuery();
+            }
+
+        }
+
+        public void cargarPosDis ()
+        {
+            string query = "SELECT puesto FROM puestos;";
+            SqlCommand comando = new SqlCommand(query, conn);
+            SqlDataReader Reader = comando.ExecuteReader();
+            while (Reader.Read())
+            {
+                checkedListBox1.Items.Add(Reader[0].ToString());
+            }
+
+            Reader.Close();
+        }
+
+        private void btnAddJug_Click(object sender, EventArgs e)
+        {
+            agregarJug("Messi", 10, 93);
+        }
+    }
+}
